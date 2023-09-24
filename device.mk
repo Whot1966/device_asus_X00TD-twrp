6e03451c4a51483859dc42e18560e2b054eedf8f
Whot1966 <Whot1966@users.noreply.github.com>
2023-09-24 20:43:19 +0700

    X00TD: Build android.hardware.keymaster@3.0 from sources

--- device.mk
+++ device.mk
@@ -38,7 +38,9 @@ PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Display
 PRODUCT_PACKAGES += \
