e439a626debda0c5f39b7cc8e60f2423211f4035
Whot1966 <Whot1966@users.noreply.github.com>
2023-09-24 20:51:10 +0700

    X00TD: Build libion from sources * Newer qseecomd need it

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    libion
 
 # Display
 PRODUCT_PACKAGES += \
